1c23ed7690f8d7e3e1f5515a8668b7bb3687682b
Lukasz <lukasz.pietrzyk1991@gmail.com>
2020-08-19 21:02:01 +0200

    content correction

--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { faLinkedin, faGithubSquare, faGoogle, IconDefinition } from "@fortawesome/free-brands-svg-icons";
+import { faAt } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.sass']
 })
 export class ContactComponent implements OnInit {
+  faLinkedin: IconDefinition = faLinkedin;
+  faGithub: IconDefinition = faGithubSquare;
+  faGoogle: IconDefinition = faGoogle;
+  faMail: IconDefinition = faAt;
 
   constructor() { }
 
